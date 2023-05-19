84bc4ebafb47f9ad2dd808f8205d4fc58d5d56ee
Zequi20 <ezequiel234242@gmail.com>
2023-05-18 20:38:49 -0400

    update db models

--- LesReserve/reserve/models.py
+++ LesReserve/reserve/models.py
@@ -6,8 +6,16 @@
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
 # Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
-# Unable to inspect table 'asigancion'
-# The error was: list index out of range
+
+
+class Ciudades(models.Model):
+    id = models.TextField(primary_key=True)  # This field type is a guess.
+    nombre = models.CharField(blank=True, null=True)
+    id_departamentos = models.ForeignKey('Departamentos', models.DO_NOTHING, db_column='id_departamentos', blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'Ciudades'
 
 
 class Clientes(models.Model):
@@ -16,24 +24,37 @@ class Clientes(models.Model):
     telefono = models.CharField(blank=True, null=True)
     direccion = models.CharField(blank=True, null=True)
     fecha_inicio = models.DateField(blank=True, null=True)
+    apellido = models.CharField(blank=True, null=True)
+    ci = models.CharField(blank=True, null=True)
+    id_ciudades = models.ForeignKey('Ciudad', models.DO_NOTHING, db_column='id_ciudades', blank=True, null=True)
+    estado = models.CharField(blank=True, null=True)
 
     class Meta:
         managed = False
-        db_table = 'clientes'
+        db_table = 'Clientes'
+
+
+class Departamentos(models.Model):
+    id = models.TextField(primary_key=True)  # This field type is a guess.
+    nombre = models.CharField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'Departamentos'
 
 
 class Habitaciones(models.Model):
-    id_hotel = models.ForeignKey('Hoteles', models.DO_NOTHING, db_column='id_hotel', blank=True, null=True)
+    id_hoteles = models.ForeignKey('Hoteles', models.DO_NOTHING, db_column='id_hoteles', blank=True, null=True)
     tipo = models.TextField(blank=True, null=True)  # This field type is a guess.
-    medidas = models.CharField(blank=True, null=True)
     numero = models.SmallIntegerField(blank=True, null=True)
     precio = models.TextField(blank=True, null=True)  # This field type is a guess.
     descripcion = models.CharField(blank=True, null=True)
     capacidad = models.TextField(blank=True, null=True)  # This field type is a guess.
+    estado = models.CharField(blank=True, null=True)
 
     class Meta:
         managed = False
-        db_table = 'habitaciones'
+        db_table = 'Habitaciones'
 
 
 class Hoteles(models.Model):
@@ -41,34 +62,52 @@ class Hoteles(models.Model):
     nombre = models.CharField(blank=True, null=True)
     direccion = models.CharField(blank=True, null=True)
     telefono = models.CharField(blank=True, null=True)
-    habitaciones = models.SmallIntegerField(blank=True, null=True)
-    pisos = models.TextField(blank=True, null=True)  # This field type is a guess.
     correo = models.CharField(blank=True, null=True)
-    ciudad = models.CharField(blank=True, null=True)
+    id_ciudad = models.ForeignKey(Ciudades, models.DO_NOTHING, db_column='id_ciudad', blank=True, null=True)
+    estado = models.CharField(blank=True, null=True)
 
     class Meta:
         managed = False
-        db_table = 'hoteles'
-# Unable to inspect table 'personal'
-# The error was: list index out of range
+        db_table = 'Hoteles'
 
 
-class Puestos(models.Model):
-    id = models.TextField(primary_key=True)  # This field type is a guess.
+class Personales(models.Model):
     nombre = models.CharField(blank=True, null=True)
+    fecha_alta = models.DateField(blank=True, null=True)
+    apellido = models.CharField(blank=True, null=True)
+    ci = models.CharField(blank=True, null=True)
+    telefono = models.CharField(blank=True, null=True)
+    correo = models.CharField(blank=True, null=True)
+    fecha_nacimiento = models.DateField(blank=True, null=True)
+    id_ciudades = models.ForeignKey(Ciudades, models.DO_NOTHING, db_column='id_ciudades', blank=True, null=True)
+    id_roles = models.ForeignKey('Roles', models.DO_NOTHING, db_column='id_roles', blank=True, null=True)
+    estado = models.CharField(blank=True, null=True)
 
     class Meta:
         managed = False
-        db_table = 'puestos'
+        db_table = 'Personales'
 
 
 class Reservas(models.Model):
-    id_cliente = models.ForeignKey(Clientes, models.DO_NOTHING, db_column='id_cliente', blank=True, null=True)
-    id_habitacion = models.ForeignKey(Habitaciones, models.DO_NOTHING, db_column='id_habitacion', blank=True, null=True)
+    id_clientes = models.ForeignKey(Clientes, models.DO_NOTHING, db_column='id_clientes', blank=True, null=True)
+    id_habitaciones = models.ForeignKey(Habitaciones, models.DO_NOTHING, db_column='id_habitaciones', blank=True, null=True)
     fecha_entrada = models.DateField(blank=True, null=True)
     fecha_salida = models.DateField(blank=True, null=True)
     precio = models.TextField(blank=True, null=True)  # This field type is a guess.
+    id_personales = models.ForeignKey(Personales, models.DO_NOTHING, db_column='id_personales', blank=True, null=True)
+    observacion = models.CharField(blank=True, null=True)
+    estado = models.CharField(blank=True, null=True)
 
     class Meta:
         managed = False
-        db_table = 'reservas'
+        db_table = 'Reservas'
+
+
+class Roles(models.Model):
+    id = models.TextField(primary_key=True)  # This field type is a guess.
+    nombre = models.CharField(blank=True, null=True)
+    descripcion = models.CharField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'Roles'
--- LesReserve/reserve/views.py
+++ LesReserve/reserve/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def vista_principal(request):
+   
+    return render(request, 'templates/index.html')
