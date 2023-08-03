5126c81fa2b0cbfb8b0053d1024b50881ae57b66
EdgarMarJara <79977475+EdgarMarJara@users.noreply.github.com>
2023-08-02 21:56:51 -0400

    tablas agregadas a la tabla admin

--- LesReserve/reserve/admin.py
+++ LesReserve/reserve/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Cliente
+from .models import Ciudad
+from .models import Departamento
+from .models import Habitacion
+from .models import Hotel
+from .models import Personal
+from .models import Reserva
 
+
+admin.site.register(Ciudad)
+admin.site.register(Departamento)
+admin.site.register(Cliente)
+admin.site.register(Habitacion)
+admin.site.register(Hotel)
+admin.site.register(Personal)
+admin.site.register(Reserva)
 # Register your models here.
