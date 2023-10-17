fc81d7a81b5be72795de9b172e87114ac23f43d4
Leila <leilabarboza2016@gmail.com>
2023-10-17 18:38:27 -0300

    Adision de clases y campos al modelo.

--- LesReserve/reserve/models.py
+++ LesReserve/reserve/models.py
@@ -8,71 +8,58 @@
 from django.db import models
 
 
-class Ciudad(models.Model):
+class Departamento(models.Model):
+    departamentoId = models.AutoField(primary_key=True)
     nombre = models.CharField(blank=True, null=True, max_length= 50)
-    id_departamento = models.ForeignKey('Departamento', on_delete=models.PROTECT, db_column='id_departamento', blank=True, null=True)
-    def __str__(self):
-        if self.id_departamento:
-            return f"{self.nombre} - {self.id_departamento.nombre}"
-        else:
-            return self.nombre
-    """ class Meta:
-        managed = False
-        db_table = 'Ciudad' """
-# Unable to inspect table 'Cliente'
-# The error was: list index out of range
+
+
+class Ciudad(models.Model):
+    ciudadId = models.AutoField(primary_key=True)
+    nombre = models.CharField(blank=True, null=True, max_length= 50)
+    id_departamento = models.ForeignKey('Departamento', on_delete=models.PROTECT, db_column='departamentoId', blank=True, null=True)
+
 
 class Cliente(models.Model):
+    clienteId = models.AutoField(primary_key=True)
     nombre = models.CharField(blank=True, null=True, max_length= 50)
     apellido = models.CharField(blank=True, null=True, max_length= 50)
-    id_ciudad = models.ForeignKey('Ciudad', db_column='id_ciudad', on_delete=models.PROTECT, blank=True, null=True)
+    id_ciudad = models.ForeignKey(Ciudad, db_column='ciudadId', on_delete=models.PROTECT, blank=True, null=True)
     correo = models.CharField(max_length=50)
     telefono = models.CharField(max_length=20)
     fecha_inicio = models.DateField()
     ci = models.CharField(max_length=20)
     estado = models.CharField(max_length=20)
     direccion = models.CharField(max_length=50)
-    def __str__(self):
-        return f"{self.nombre} - {self.apellido} - {self.id_ciudad.nombre} - {self.correo}"
-    
-
-
-
-
-class Departamento(models.Model):
-    nombre = models.CharField(blank=True, null=True, max_length= 50)
-    def __str__(self):
-        return self.nombre
-    
-  
-
-class Habitacion(models.Model):
-    id_hotel = models.ForeignKey('Hotel', on_delete=models.PROTECT, db_column='id_hotel', blank=True, null=True)
-    tipo = models.CharField(blank=True, null=True, max_length= 50)  # This field type is a guess.
-    numero = models.SmallIntegerField(blank=True, null=True)
-    precio = models.PositiveBigIntegerField() # This field type is a guess.
-    descripcion = models.CharField(blank=True, null=True, max_length= 50)
-    capacidad = models.PositiveIntegerField()  # This field type is a guess.
-    estado = models.CharField(blank=True, null=True, max_length=20)
-
-    def __str__(self):
-        return f"{self.numero} - {self.id_hotel.nombre} - {self.tipo} - precio: {self.precio} - {self.descripcion} - {self.capacidad} personas - {self.estado}"
-
+    foto = models.CharField(blank=True, null=True, max_length= 500)
 
 
 class Hotel(models.Model):
+    hotelId = models.AutoField(primary_key=True)
     nombre = models.CharField(blank=True, null=True, max_length=50)
     direccion = models.CharField(blank=True, null=True, max_length=50)
     telefono = models.CharField(blank=True, null=True, max_length=50)
     correo = models.CharField(blank=True, null=True, max_length=50)
-    id_ciudad = models.ForeignKey(Ciudad, on_delete=models.PROTECT, db_column='id_ciudad', blank=True, null=True)
+    id_ciudad = models.ForeignKey(Ciudad, on_delete=models.PROTECT, db_column='ciudadId', blank=True, null=True)
     estado = models.CharField(blank=True, null=True, max_length=20)
-    def __str__(self):
-        return f"{self.nombre}"
-   
+
+  
+
+class Habitacion(models.Model):
+    habitacionId = models.AutoField(primary_key=True)
+    id_hotel = models.ForeignKey('Hotel', on_delete=models.PROTECT, db_column='id_hotel', blank=True, null=True)
+    tipo = models.CharField(blank=True, null=True, max_length= 50)  # This field type is a guess.
+    numero = models.SmallIntegerField(blank=True, null=True)
+    precio = models.PositiveBigIntegerField() # This field type is a guess.
+    descripcion = models.CharField(blank=True, null=True, max_length= 500)
+    foto = models.CharField(blank=True, null=True, max_length= 500)
+    capacidad = models.PositiveIntegerField()  # This field type is a guess.
+    estado = models.CharField(blank=True, null=True, max_length=20)
+
 
 
 class Personal(models.Model):
+    personalId = models.AutoField(primary_key=True)
+    personalId = models.AutoField(primary_key=True)
     nombre = models.CharField(blank=True, null=True, max_length=50)
     apellido = models.CharField(blank=True, null=True, max_length=50)
     fecha_alta = models.DateField(blank=True, null=True)
@@ -82,15 +69,21 @@ class Personal(models.Model):
     telefono = models.CharField(blank=True, null=True, max_length=50)
     correo = models.CharField(blank=True, null=True, max_length=50)
     fecha_nacimiento = models.DateField(blank=True, null=True)
-    id_ciudad = models.ForeignKey(Ciudad, on_delete=models.PROTECT, db_column='id_ciudad', blank=True, null=True)
+    id_ciudad = models.ForeignKey(Ciudad, on_delete=models.PROTECT, db_column='ciudadId', blank=True, null=True)
     estado = models.CharField(blank=True, null=True, max_length=50)
-    def __str__(self):
-        return f"{self.ci} - {self.nombre} {self.apellido} - {self.puesto} - {self.telefono} - {self.correo}"
+    foto = models.CharField(blank=True, null=True, max_length= 500)
+
+class Servicio(models.Model):
+    servicioId = models.AutoField(primary_key=True)
+    precio = models.PositiveBigIntegerField() # This field type is a guess.
+    servicio_tipo = models.CharField(blank=True, null=True, max_length= 500)
+    detalle = models.CharField(blank=True, null=True, max_length=500)
 
 
 class Reserva(models.Model):
-    
+    reservaId = models.AutoField(primary_key=True)
     id_cliente = models.ForeignKey(Cliente, on_delete=models.PROTECT, db_column='id_cliente', blank=True, null=True)
+    id_servicio = models.ForeignKey(Servicio, on_delete=models.PROTECT, db_column='id_servicio', blank=True, null=True)
     id_habitacion = models.ForeignKey(Habitacion, on_delete=models.PROTECT, db_column='id_habitacion', blank=True, null=True)
     id_personal = models.ForeignKey(Personal, on_delete=models.PROTECT, db_column='id_personal', blank=True, null=True)
     cantidad = models.SmallIntegerField(default=0)
@@ -98,8 +91,12 @@ class Reserva(models.Model):
     fecha_fin = models.DateField(blank=True, null=True)
     estado = models.CharField(blank=True, null=True, max_length=50)
     observacion = models.CharField(blank=True, null=True, max_length=500)
-    def __str__ (self):
-        return f"{self.id_cliente.nombre} - habitacion {self.id_habitacion.numero} - {self.cantidad} personas"
+ 
 
+class Resena(models.Model):
+    resenaId = models.AutoField(primary_key=True)
+    id_hotel = models.ForeignKey('Hotel', on_delete=models.PROTECT, db_column='id_hotel', blank=True, null=True)
+    id_cliente = models.ForeignKey(Cliente, on_delete=models.PROTECT, db_column='id_cliente', blank=True, null=True)
+    comentario = models.CharField(blank=True, null=True, max_length=500)
 
 
