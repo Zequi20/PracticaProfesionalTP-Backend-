26a0f1c316ab3f18fe4e1794244a70e0ad70d79b
EdgarMarJara <79977475+EdgarMarJara@users.noreply.github.com>
2023-11-10 16:29:01 -0300

    Entrega parte practica segunda parcial.

--- LesReserve/reserve/tests.py
+++ LesReserve/reserve/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class InicioVistaTest(TestCase):
+    def test_inicio_devuelve_200(self):
+        url = reverse('vista_inicio')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_inicio_no_vacia(self):
+        url = reverse('vista_inicio')
+        response = self.client.get(url)
+        self.assertContains(response, '<ul', count=1)
+
+class ClientesVistaTest(TestCase):
+
+    def test_clientes_devuelve_200(self):
+        url = reverse('vista_clientes') 
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_clientes_no_vacia(self):
+        url = reverse('vista_clientes')
+        response = self.client.get(url)
+        self.assertContains(response, '<ul', count=1)
+
+class ReservasVistaTest(TestCase):
+    def test_reservas_devuelve_200(self):
+        url = reverse('vista_reservas') 
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_reservas_no_vacia(self):
+        url = reverse('vista_reservas')
+        response = self.client.get(url)
+        self.assertContains(response, '<ul', count=1)
