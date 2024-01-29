9481862b4ed71a775b4d667a7af2a268a4d8a48c
Leila <leilabarboza2016@gmail.com>
2024-01-29 17:50:01 -0300

    registro de la aplicación y módulos

--- LesReserve/LesReserve/settings.py
+++ LesReserve/LesReserve/settings.py
@@ -32,16 +32,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'reserve',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'reserve.apps.ReserveConfig'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
