f8ad4ab4bfae01355eaa0d71f4eba7da92d1f9df
Zequi20 <ezequiel234242@gmail.com>
2024-02-16 00:32:34 -0300

    agregados metodos post a cada vista

--- LesReserve/reserve/views.py
+++ LesReserve/reserve/views.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .models import Cliente
 from .models import Ciudad
 from .models import Departamento
@@ -21,12 +22,26 @@ class ClienteView(APIView):
         queryset = Cliente.objects.all()
         serializer = ClienteSerializer(queryset, many=True)
         return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = ClienteSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class CiudadView(APIView):
     def get(self, request):
         queryset = Ciudad.objects.all()
         serializer = CiudadSerializer(queryset, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CiudadSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 class DepartamentoView(APIView):
     def get(self, request):
@@ -34,11 +49,25 @@ class DepartamentoView(APIView):
         serializer = DepartamentoSerializer(queryset, many=True)
         return Response(serializer.data)
     
+    def post(self, request):
+        serializer = DepartamentoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 class HabitacionView(APIView):
     def get(self, request):
         queryset = Habitacion.objects.all()
         serializer = HabitacionSerializer(queryset, many=True)
         return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = HabitacionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUE)
     
 class HotelView(APIView):
     def get(self, request):
@@ -46,14 +75,35 @@ class HotelView(APIView):
         serializer = HotelSerializer(queryset, many=True)
         return Response(serializer.data)
     
+    def post(self, request):
+        serializer = HotelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUE)
+    
 class PersonalView(APIView):
     def get(self, request):
         queryset = Personal.objects.all()
         serializer = PersonalSerializer(queryset, many=True)
         return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = PersonalSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUE)
 
 class ReservaView(APIView):
     def get(self, request):
         queryset = Reserva.objects.all()
         serializer = ReservaSerializer(queryset, many=True)
         return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = ReservaSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUE)
