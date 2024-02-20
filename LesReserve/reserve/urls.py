530f75753c77c2c29e05649e4d63d9f2a1103f52
Leila <leilabarboza2016@gmail.com>
2024-02-20 14:21:54 -0300

    path para servicios y resenha

--- LesReserve/reserve/urls.py
+++ LesReserve/reserve/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ClienteView, CiudadView, DepartamentoView, HabitacionView, HotelView, PersonalView, LoginView
+from .views import ClienteView, CiudadView, DepartamentoView, HabitacionView, HotelView, PersonalView, ServicioView , ResenaView, LoginView
 
 urlpatterns = [
     path('clientes', ClienteView.as_view(), name='api-clientes'),
@@ -8,5 +8,9 @@ urlpatterns = [
     path('habitaciones', HabitacionView.as_view(), name='api-habitaciones'),
     path('hoteles', HotelView.as_view(), name='api-hoteles'),
     path('personal', PersonalView.as_view(), name='api-personal'),
+
+    path('servicios', ServicioView.as_view(), name='api-servicios'),
+    path('resenas', ResenaView.as_view(), name='api-resena'),
+
     path('login', LoginView.as_view(), name='api-login'),
 ]
